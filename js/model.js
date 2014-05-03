2e5f47210552cba5a43c02654d4a2237fdfca952
Son Pham <son@esplorio.com>
2014-05-03 23:50:22 +0100

    More styling. Add Add/Remove items button

--- js/model.js
+++ js/model.js
@@ -10,6 +10,7 @@
  * post := { price, daysAgo, description }
  */
 var posts = [
+    /* Sale posts */
     {
         price: "£100",
         ago: "2 days ago",
@@ -35,7 +36,9 @@ var posts = [
         ago: "5 days ago",
         description: "<b>Macbook Pro 2013 Like New!</b> Will post to anywhere in the UK. Including free Apple Magic Mouse and spare charger to sweeten the deal, plus <b>6 months left on Apple Care!</b>",
         type: "sale"
-    }, {
+    }, 
+    /* Wanted posts */
+    {
         price: "£7/h+tips",
         ago: "today",
         description: "<b>Waiter needed</b> for a Vietnamese restaurant in South London. Valid ID and work permit (especially students) required. No experience needed, training will be included. Both full-time and part-time are welcomed.",
@@ -50,31 +53,43 @@ var posts = [
         ago: "1 day ago",
         description: "<b>Looking for a home tutor</b> to teach Vietnamese to kids. <b>Two 2-hour lessons a week</b> Professional preferred, but students will also be considered.",
         type: "wanted"
-    }, {
-        price: "500pcm",
+    },
+    /* Rent posts */
+    {
+        price: "£500pcm",
         ago: "2 days ago",
         description: "<b>Double room for rent</b> in Hackney E2. Suitable for couples. Very close to large supermarkets (Tesco, Asda), Vietnamese shops and restaurants. Buses: <b>149, 67, 242</b>. Nearby stations: <b>Hoxton, Shoreditch High Street</b>",
         type: "rent"
     }, {
-        price: "450pcm",
+        price: "£450pcm",
         ago: "2 days ago",
         description: "<b>Double room</b>. Can be shared for up to 2 people in <b>Finsbury Park, North London</b>. 5 mins walk to Sainsbury and 15 mins away from Finsbury Park Station. <b>Female students only</b>",
         type: "rent"
     }, {
-        price: "500pcm",
+        price: "£500pcm",
         ago: "2 days ago",
         description: "<b>Large studio room</b> in a Vietnamese family's large house, <b>all bills included!!</b> Post code: E3. Conveniently located near large shopping mall and public transport. <b>Students and professionals both welcome</b>.",
         type: "rent"
     }, {
-        price: "2000pcm",
+        price: "£2000pcm",
         ago: "2 days ago",
         description: "<b>1-bedroom apartment</b>, Canada Water. Central area with all large shops, London's financial offices and major public transport links within walking distance. <b>Best suited for a single professional</b>.",
         type: "rent"
     }, {
-        price: "1500pcm",
+        price: "£1500pcm",
         ago: "1 day ago",
         description: "<b>4-bedroom house</b> Greenwich SE10. Very near to Greenwich uni. Close to Greenwich station, Tesco and Chinese shop. Viewing arrangements welcomed.",
         type: "rent"
+    },
+    /* Longdan posts */
+    {
+        id: 1,
+        price: "£20.00",
+        desc1: "Imperial Rice Vermicelli 1.6mm",
+        desc2: "Bún Hoàng Gia 1.6mm",
+        image_url: "http://www.longdan.co.uk/uploads/sanpham/prod_260213094726.jpg",
+        packaging: "400g x 30 bags",
+        type: "longdan"
     }
 ];
 
@@ -200,4 +215,29 @@ app.controller("ListingsCtrl", function($scope, $timeout) {
     
     $scope.notifications = notifications;
     
+    
+    /* Longdan */
+    $scope.basket = {};
+    
+    $scope.addBasket = function(id) {
+        if (id in $scope.basket) {
+            $scope.basket[id] += 1;
+        } else {
+            $scope.basket[id] = 1;
+        }
+    };
+    
+    $scope.removeBasket = function(id) {
+        if (id in $scope.basket && $scope.basket[id] > 0) {
+            $scope.basket[id] -= 1;
+        }
+    };
+    
+    $scope.showQuantity = function(id) {
+        if (id in $scope.basket) {
+            return $scope.basket[id];
+        } else {
+            return 0;
+        }
+    };
 });
