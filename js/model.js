03526bb7db029315f2ab94f36b6e6c701d0cc5a9
Son Pham <son@esplorio.com>
2014-05-03 17:22:11 +0100

    More posts

--- js/model.js
+++ js/model.js
@@ -11,52 +11,67 @@
  */
 var posts = [
     {
-        price: 100,
+        price: "£100",
         ago: "2 days ago",
         description: "<b>Old heater</b>, good for winter holidays. Heats well even in coldest weather for a bedroom-sided room. Will send anywhere within UK, <b>delivery</b> included.",
         type: "sale"
     }, {
-        price: 150,
+        price: "£150",
         ago: "4 days ago",
         description: "Old <b>50-inch TV</b>, brand Panasonic, black, small bezel. Purchased in 2009, in good condition (see photos). Has an in-built DVD player, 2 USB, 3 HDMI, Component and 2 AVI. <b>LED</b> display, may not good against direct sunlight. Delivery costs not included, please pick up from Clapham, London.",
         type: "sale"
     }, {
-        price: 400,
+        price: "£400",
         ago: "2 days ago",
         description: "<b>Phone 5S gold colour</b>. Barely used. Deliver anywhere within UK (delivery not included, up to £5 extra). Will throw in a free cover of your choice (see photos).",
         type: "sale"
     }, {
-        price: 150,
+        price: "£150",
         ago: "6 days ago",
-        description: "<b>Brand new female bike</b>. Selling due to lack of use, collect in person only.",
+        description: "<b>Brand new female bike</b> in London! Raleigh bike, including lights, lock and a free front basket. Recently serviced. Selling due to lack of use, collect in person only.",
         type: "sale"
     }, {
-        price: 750,
+        price: "£750",
         ago: "5 days ago",
         description: "<b>Macbook Pro 2013 Like New!</b> Will post to anywhere in the UK. Including free Apple Magic Mouse and spare charger to sweeten the deal, plus <b>6 months left on Apple Care!</b>",
         type: "sale"
     }, {
-        price: '500pcm',
+        price: "£7/h+tips",
+        ago: "today",
+        description: "<b>Waiter needed</b> for a Vietnamese restaurant in South London. Valid ID and work permit (especially students) required. No experience needed, training will be included. Both full-time and part-time are welcomed.",
+        type: "wanted"
+    }, {
+        price: "Wanted!",
+        ago: "1 day ago",
+        description: "<b>Man and van wanted!</b> We are 4 students and need to move to our new place across town in London early in June. Prices can be negotiable!",
+        type: "wanted"
+    }, {
+        price: "£20/h",
+        ago: "1 day ago",
+        description: "<b>Looking for a home tutor</b> to teach Vietnamese to kids. <b>Two 2-hour lessons a week</b> Professional preferred, but students will also be considered.",
+        type: "wanted"
+    }, {
+        price: "500pcm",
         ago: "2 days ago",
         description: "<b>Double room for rent</b> in Hackney E2. Suitable for couples. Very close to large supermarkets (Tesco, Asda), Vietnamese shops and restaurants. Buses: <b>149, 67, 242</b>. Nearby stations: <b>Hoxton, Shoreditch High Street</b>",
         type: "rent"
     }, {
-        price: '450pcm',
+        price: "450pcm",
         ago: "2 days ago",
         description: "<b>Double room</b>. Can be shared for up to 2 people in <b>Finsbury Park, North London</b>. 5 mins walk to Sainsbury and 15 mins away from Finsbury Park Station. <b>Female students only</b>",
         type: "rent"
     }, {
-        price: '500pcm',
+        price: "500pcm",
         ago: "2 days ago",
         description: "<b>Large studio room</b> in a Vietnamese family's large house, <b>all bills included!!</b> Post code: E3. Conveniently located near large shopping mall and public transport. <b>Students and professionals both welcome</b>.",
         type: "rent"
     }, {
-        price: '2000pcm',
+        price: "2000pcm",
         ago: "2 days ago",
         description: "<b>1-bedroom apartment</b>, Canada Water. Central area with all large shops, London's financial offices and major public transport links within walking distance. <b>Best suited for a single professional</b>.",
         type: "rent"
     }, {
-        price: '1500pcm',
+        price: "1500pcm",
         ago: "1 day ago",
         description: "<b>4-bedroom house</b> Greenwich SE10. Very near to Greenwich uni. Close to Greenwich station, Tesco and Chinese shop. Viewing arrangements welcomed.",
         type: "rent"
