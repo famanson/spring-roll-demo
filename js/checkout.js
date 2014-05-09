dbfe0929a14db4fb61a5b3b727fa4b58ff6ad078
Son Pham <son@esplorio.com>
2014-05-09 22:00:17 +0100

    longdan into another file

---
+++ js/checkout.js
@@ -0,0 +1,146 @@
+var ldCategories = [
+    { name: "Fresh & Chilled" },
+    { name: "Rice & Noodles"},
+    { name: "Seasoning & Spices"},
+    { name: "Confectionery"},
+    { name: "Drinks"},
+    { name: "Non-food"}
+ ];
+
+/* Longdan */
+app.controller("CheckoutCtrl", function($scope) {
+    $scope.longdanEnabled = true;
+    if (!$scope.longdanEnabled) {
+        $scope.topNavs.pop();
+    } else {
+        $scope.basket = {};
+
+        $scope.addBasket = function(id) {
+            if (id in $scope.basket) {
+                $scope.basket[id] += 1;
+            } else {
+                $scope.basket[id] = 1;
+            }
+        };
+
+        $scope.removeBasket = function(id) {
+            if (id in $scope.basket && $scope.basket[id] > 0) {
+                $scope.basket[id] -= 1;
+            }
+        };
+
+        $scope.showQuantity = function(id) {
+            if (id in $scope.basket) {
+                return $scope.basket[id];
+            } else {
+                return 0;
+            }
+        };
+
+        // First work out Longdan's prices
+        var longdanPosts = posts.filter(function(post) {return post.type == 'longdan';});
+        var longdanPrices = {};
+        for (var j = 0; j < longdanPosts.length; j++) {
+            // Convert prices to numeric values here - this was not done in the definition because it'd
+            // mess up the formatting
+            var numericPrice = parseFloat(longdanPosts[j].price.replace('£',''));
+            longdanPrices[longdanPosts[j].id] = numericPrice;
+        }
+
+        $scope.deliveryFee = 15;
+
+        $scope.basketTotal = function() {
+            var total = 0;
+            for (var key in $scope.basket) {
+                // $scope.basket[key] would be the quantity
+                var price = longdanPrices[key];
+                total += price * $scope.basket[key];
+            }
+            if (total >= 100) {
+                $scope.deliveryFee = 0;
+            } else {
+                $scope.deliveryFee = 15;
+            }
+            return total;
+        };
+
+        $scope.showTruck = function() {
+            return $scope.isDeliveryFree() ? 'free-truck.png' : 'truck.png';
+        };
+
+        $scope.deliveryFeeDisplay = function() {
+            return $scope.isDeliveryFree() ? 'FREE!' : "£" + $scope.deliveryFee.toFixed(2);
+        };
+
+        $scope.isDeliveryFree = function() {
+            return $scope.deliveryFee === 0;
+        };
+
+        // Longdan images for use in the checkout overlay
+        var longdanImages = {};
+        for (var k = 0; k < longdanPosts.length; k++) {
+            longdanImages[longdanPosts[k].id] = {
+                imageUrl: longdanPosts[k].image_url,
+                description: longdanPosts[k].desc1
+            };
+        }
+        $scope.longdanImages = longdanImages;
+
+        $scope.checkedOutItems = [];
+        $scope.showCheckoutOverlay = false;
+        $scope.prepareCheckout = function () {
+            var checkedOutItems = [];
+            for (var key in $scope.basket) {
+                // $scope.basket[key] would be the quantity
+                var quantity = $scope.basket[key];
+                var price = longdanPrices[key] * quantity;
+                var imageUrl = longdanImages[key].imageUrl;
+                var description = longdanImages[key].description;
+                if (quantity > 0) {
+                    checkedOutItems.push({
+                        quantity: quantity,
+                        price: price,
+                        imageUrl: imageUrl,
+                        description: description,
+                    });
+                }
+            }
+            $scope.checkedOutItems = checkedOutItems;
+        };
+        // Checkout basket clicked
+        $scope.toggleCheckoutClicked = false;
+        $scope.toggleCheckout = function() {
+            $scope.toggleCheckoutClicked = true;
+            $scope.checkoutSelected = !$scope.checkoutSelected;
+        };
+        // Dismiss checkout overlay when clicking elsewhere
+        $scope.dismissCheckout = function() {
+            if (!$scope.toggleCheckoutClicked) {
+                $scope.checkoutSelected = false;
+            }
+            $scope.toggleCheckoutClicked = false;
+        };
+        // Clicks on the overlay itself will override dismiss and won't make it go away
+        $scope.forceCheckoutOn = function () {
+            if (!$scope.checkoutClosed) {
+                $scope.toggleCheckoutClicked = true;
+            }
+            // Reset the Close button
+            $scope.checkoutClosed = false;
+        };
+        // Clicks on the Close button will override the overlay click!
+        $scope.checkoutClosed = false;
+        $scope.forceCheckoutOff = function () {
+            $scope.toggleCheckoutClicked = false;
+            $scope.checkoutClosed = true;
+        };
+
+        $scope.checkoutProgress = function() {
+            var fraction = $scope.basketTotal()/50;
+            return Math.round(fraction*100) + "%";
+        };
+
+
+        $scope.ldCategories = ldCategories;
+    }
+});
