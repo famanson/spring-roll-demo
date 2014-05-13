70216a08588fbace28fa6fc3141a5c05c28c1009
Son Pham <me@famanson.com>
2014-05-13 23:09:04 +0100

    More submit button behaviour

--- js/compose.js
+++ js/compose.js
@@ -16,27 +16,36 @@ app.controller("ComposeCtrl", function($scope) {
         $scope.submittedDesc = "";
         $scope.pickedCategory = "";
     };
+    // Call clear once to init everything
+    $scope.clearSubmit();
+    // Work out expiration date
     var monthNames = [ "January", "February", "March", "April", "May", "June",
                        "July", "August", "September", "October", "November", "December" ];
     var expireDate = new Date(+new Date + 12096e5);
     $scope.fortnightAway = expireDate.getDay() + " " + monthNames[expireDate.getMonth()] + " " + expireDate.getFullYear();
-    $scope.submitPost = function() {
-        var popType = $scope.pickedCategory.toLowerCase();
-        var post = {
-            price: $scope.submittedPrice,
-            ago: "a moment ago",
-            description: $scope.submittedDesc,
-            type: popType
-        };
-        // Hack!
-        var composePost = posts[0];
-        posts.splice(0,1);
-        posts.unshift(post);
-        posts.unshift(composePost);
-        // End hack
 
-        $scope.emptyColumns();
-        $scope.populateByType(popType);
-        $scope.composeBoxEnabled = false;
+    $scope.isOkayToSubmit = function() {
+        return $scope.pickedCategory.toLowerCase().length > 0 && $scope.submittedPrice.length > 0 && $scope.submittedDesc.length > 0;
+    };
+    $scope.submitPost = function() {
+        if ($scope.isOkayToSubmit()) {
+            var popType = $scope.pickedCategory.toLowerCase();
+            var post = {
+                price: $scope.submittedPrice,
+                ago: "a moment ago",
+                description: $scope.submittedDesc,
+                type: popType
+            };
+            // Hack!
+            var composePost = posts[0];
+            posts.splice(0,1);
+            posts.unshift(post);
+            posts.unshift(composePost);
+            // End hack
+
+            $scope.emptyColumns();
+            $scope.populateByType(popType);
+            $scope.composeBoxEnabled = false;
+        }
     };
 });
