029371d36372aeada453614aa2e36496ce5adbd5
Son Pham <me@famanson.com>
2014-05-13 21:29:03 +0100

    Form submit

--- js/compose.js
+++ js/compose.js
@@ -1,9 +1,17 @@
 /*global app */
 
 app.controller("ComposeCtrl", function($scope) {
+    $scope.composeCategories = ['Sale','Wanted','Rent'];
     $scope.composeBoxEnabled = false;
     
     $scope.setComposeBoxEnabled = function(enabled) {
         $scope.composeBoxEnabled = enabled;
+    };
+    $scope.pickedCategory = "";
+    $scope.setPickedCategory = function(category) {
+        $scope.pickedCategory = category;
+    };
+    $scope.submitPost = function() {
+        alert(this.submittedPrice + ", " + this.submittedDesc + ", " + $scope.pickedCategory);
     };
 });
