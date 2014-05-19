5bcb6f9204f5e9ea4ba50ad82295fe77b291f943
Son Pham <me@famanson.com>
2014-05-19 21:05:21 +0100

    [test] Workaround for less agressive caching

--- js/misc.js
+++ js/misc.js
@@ -11,3 +11,23 @@ $(document).on('scroll', function() {
         $('#ld-header').removeClass("fixed");
     }
 });
+
+/* Workaround for LESS local storage cache problem */
+function destroyLessCache(pathToCss) { // e.g. '/css/' or '/stylesheets/'
+  if (!window.localStorage || !less || less.env !== 'development') {
+    return;
+  }
+  var host = window.location.host;
+  var protocol = window.location.protocol;
+  var keyPrefix = protocol + '//' + host + pathToCss;
+
+  for (var key in window.localStorage) {
+    if (key.indexOf(keyPrefix) === 0) {
+      delete window.localStorage[key];
+    }
+  }
+}
+
+$(document).ready(function() {
+    destroyLessCache("/styles/");
+});
