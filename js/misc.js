d772f788e065e5545a5909e691caf2062f422339
Son Pham <me@famanson.com>
2014-05-23 19:37:00 +0100

    Mailgun email address validator

--- js/misc.js
+++ js/misc.js
@@ -2,7 +2,14 @@
 
 $(document).ready(function() {
     $(".bottom").append('<input name="chbox-captcha" type="checkbox"><p>Pinky-promise I\'m not a bot!</p>');
+//    $("#email-address").mailgun_validator({
+//        api_key: "pubkey-5nc-amvnbfw1j1k8w45usr5w36y-job2",
+//        success: function(){alert("Success");},
+//        error: function(){alert("Failure");},
+//    });
 });
+
+
 
 /* Fixes the category bar in Longdan tab */
 
