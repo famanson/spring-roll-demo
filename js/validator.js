0e411797118bf38e341dbb9161911037d405e78c
Son Pham <me@famanson.com>
2014-05-23 21:11:14 +0100

    Progress image and input autogrow

--- js/validator.js
+++ js/validator.js
@@ -8,17 +8,52 @@ app.directive('ngValidator', function() {
         // responsible for registering DOM listeners as well as updating the DOM
         link: function(scope, element, attrs) {
             var processResult = function(data) {
+                scope.validationProgress = false;
+                var text = "";
+                if (!data.is_valid) {
+                    if (data.did_you_mean != null && data.did_you_mean.length > 0) {
+                        text = "Did you mean \"" + data.did_you_mean + "\"?";
+                    } else {
+                        text = "Address is invalid";
+                    }
+                } else {
+                    text = "This looks good!";
+                }
                 scope.validationResult = {
                     valid: data.is_valid,
-                    suggestion: data.did_you_mean
+                    text: text
                 };
+                scope.$digest();
+            };
+            var errorResult = function() {
+                scope.validationProgress = false;
+                scope.validationResult = {
+                    valid: false,
+                    text: "Address is invalid"
+                };
+                scope.$digest();
+            };
+            var progress = function() {
+                scope.validationProgress = true;
                 scope.$digest();
             };
             $(element).mailgun_validator({
                 api_key: "pubkey-5nc-amvnbfw1j1k8w45usr5w36y-job2",
                 success: processResult,
-                error: function(){},
+                error: errorResult,
+                in_progress: progress
             });
+        }
+    };
+});
+
+app.directive('ngAutogrow', function() {
+    return {
+        // Restrict it to be an attribute in this case
+        restrict: 'A',
+        // responsible for registering DOM listeners as well as updating the DOM
+        link: function(scope, element, attrs) {
+            $(element).autoGrow();
         }
     };
 });
