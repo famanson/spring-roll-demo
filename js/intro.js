086ca9eb96f7ba22b884a916c0bd6ef76c18dd5b
Son Pham <me@famanson.com>
2014-06-02 20:50:25 +0100

    CORS support for the PHP calls

--- js/intro.js
+++ js/intro.js
@@ -1,6 +1,20 @@
 /* jshint browser:true, jquery:true */
 /* global angular, app */
 
+app.config(function($httpProvider) {
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+    $httpProvider.defaults.headers.post["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept";
+    $httpProvider.defaults.headers.post["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, DELETE, PUT, HEAD";
+
+
+    //Remove the header containing XMLHttpRequest used to identify ajax call 
+    //that would prevent CORS from working
+    // delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+
 app.controller("IntroCtrl", function($scope, $http) {
     $scope.isHuman = function() {
         var chboxCaptcha = angular.element('input[name="chboxCaptcha"]').is(':checked'),
@@ -42,36 +56,34 @@ app.controller("IntroCtrl", function($scope, $http) {
         $scope.messageResult = ""
         $scope.showMessageResult = false;
     };
+    $scope.relayPath = window.relayPath || "http://famanson.com/_sr/relay.php";
     $scope.send = function() {
-        if ($scope.isReadyToSend()) {
-            if ($scope.isHuman()) {
-                // $scope.setMessageFormVisible(false);
-                $http.post("./relay.php", {
-                    email: $scope.email,
-                    messageBody: $scope.messageBody,
-                }).success(function(data, status, headers, config) {
-                    $scope.email = "";
-                    $scope.messageBody = "";
-                    $scope.validationResult = null;
-                    $scope.showMessageResult = true;
-                    angular.element('input[name="chboxCaptcha"]').attr('checked', false);
-                    $scope.messageForm.$setPristine(true);
-                    $scope.messageResult = {
-                        text: "Message has been sent!",
-                        error: false
-                    };
-                    $scope.messageFade = { opacity: 0, display: 'none' };
-                    $scope.messageResultFade = { opacity: 1 };
-                }).error(function(data, status, headers, config) {
-                    $scope.showMessageResult = true;
-                    $scope.messageResult = {
-                        text: "Error found. Please retry later!",
-                        error: true
-                    };
-                    $scope.messageFade = { opacity: 1, display: 'block' };
-                    $scope.messageResultFade = { opacity: 0 };
-                });
-            }
+        if ($scope.isReadyToSend() && $scope.isHuman()) {
+            $http.post($scope.relayPath, {
+                email: $scope.email,
+                messageBody: $scope.messageBody,
+            }).success(function(data, status, headers, config) {
+                $scope.email = "";
+                $scope.messageBody = "";
+                $scope.validationResult = null;
+                $scope.showMessageResult = true;
+                angular.element('input[name="chboxCaptcha"]').attr('checked', false);
+                $scope.messageForm.$setPristine(true);
+                $scope.messageResult = {
+                    text: "Message has been sent!",
+                    error: false
+                };
+                $scope.messageFade = { opacity: 0, display: 'none' };
+                $scope.messageResultFade = { opacity: 1 };
+            }).error(function(data, status, headers, config) {
+                $scope.showMessageResult = true;
+                $scope.messageResult = {
+                    text: "Error found. Please retry later!",
+                    error: true
+                };
+                $scope.messageFade = { opacity: 1, display: 'block' };
+                $scope.messageResultFade = { opacity: 0 };
+            });
         }
     };
 });
